65ea025bc548e0041e95552033597c39747c27f3
Joshua <jwegahub@gmail.com>
2023-09-25 09:45:37 +0300

    updated date-percentage logic and added comments

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 //simple program that takes and expense sheet and visualizes statistics for you
-//NOTE: syntax is very important. The top row should have the values: |Expense |Amount |Date |. Then fill in the columns
+//NOTE: syntax is very important. The top row should have the values: |Expense |Amount |Date |. Then fill in the columns. 
+//NOTE: For entries in the expense column use this format: "expense name ("expense variant")", if there is no variant then parenthesis are not needed. e.g "Amazon purchase (RTX 4090)" 
 //NOTE: Date format is important for the function 'findDatePercentage' xlsx files already have built in formating for dates it is important that you follow them.
 
+
+//import xlsx
 const XLSX = require("xlsx");
-const workbook = XLSX.readFile("expenses.xlsx", { cellDates: true });
-const ws1 = workbook.Sheets["0923"];
+
+//create workbook from your .xlsx file
+//cell dates is so that dates for all the entries are in js date format
+const workbook = XLSX.readFile("expenses.xlsx" /** replace with your own file path */, { cellDates: true });
+
+const ws1 = workbook.Sheets["Sheet1" /**select worksheet from work  book e.g "Sheet1" */];
+
+//convert sheet data to json format using 'sheet to json'
 const data = XLSX.utils.sheet_to_json(ws1);
+
+//import utils
 const utils = require("./utils");
 
+//total for all expenses
 let total = 0;
 
+//loop through the data and change it to the desired format
 data.forEach((element) => {
   element.variant = String(element["Expense"])
     .substring(
@@ -23,10 +36,11 @@ data.forEach((element) => {
       String(element.Expense).indexOf("(") - 1
     );
   }
-
   element.Expense = String(element.Expense).toLocaleLowerCase();
   total += element.Amount;
 });
 
+
 data.total = total;
-console.table(utils.findDatePercentage(data));
+
+console.log(utils.findDatePercentage(data));
--- utils.js
+++ utils.js
@@ -41,18 +41,64 @@ module.exports.findExpensePercentage = (data) => {
   return results;
 };
 
-module.exports.findDatePercentage = (data) =>
-{
-    let results = {}
-    data.forEach((element) =>
-    {
-        results[element.Date.toDateString()] = {total:0}
-    })
-    data.forEach((element) =>
-    {
-        results[element.Date.toDateString()].total += element.Amount
-        results[element.Date.toDateString()].percent = `${((results[element.Date.toDateString()].total / data.total) * 100).toFixed(1)}%`
-    })
+module.exports.findDatePercentage = (data) => {
+  let results = {};
 
-    return results
-}
+  //initialize  the total for each
+  data.forEach((element) => {
+    results[element.Date.toDateString()] = { total: 0 };
+  });
+
+  //calculate the total and percentage for each date
+  //initialize the amount for each expense
+  data.forEach((element) => {
+    results[element.Date.toDateString()].total += element.Amount;
+    results[element.Date.toDateString()].percent = `${(
+      (results[element.Date.toDateString()].total / data.total) *
+      100
+    ).toFixed(1)}%`;
+    results[element.Date.toDateString()] = {
+      ...results[element.Date.toDateString()],
+      [element.Expense]: { amount: 0 },
+    };
+  });
+
+  //calculate the total and percent for each expense
+  //initialize the amount for each expense variant
+  data.forEach((element) => {
+    results[element.Date.toDateString()][element.Expense].amount +=
+      element.Amount;
+    results[element.Date.toDateString()][element.Expense].percent = `${(
+      (results[element.Date.toDateString()][element.Expense].amount /
+        results[element.Date.toDateString()].total) *
+      100
+    ).toFixed(1)}%`;
+    if (element.variant) {
+      results[element.Date.toDateString()][element.Expense][element.variant] = {
+        amount: 0,
+        percent: {},
+      };
+    }
+
+    
+  });
+
+  //calculate the amount and percent for each expense variant
+  data.forEach((element) => 
+   {
+    if (element.variant) {
+      results[element.Date.toDateString()][element.Expense][element.variant].amount += element.Amount
+      results[element.Date.toDateString()][element.Expense][element.variant].percent.relative = `${(
+        (results[element.Date.toDateString()][element.Expense][element.variant].amount / results[element.Date.toDateString()][element.Expense].amount) *
+        100
+      ).toFixed(1)}%`; 
+      results[element.Date.toDateString()][element.Expense][element.variant].percent.absolute = `${(
+        (results[element.Date.toDateString()][element.Expense][element.variant].amount / results[element.Date.toDateString()].total) *
+        100
+      ).toFixed(1)}%`; 
+    }
+   })
+
+   return results
+
+};
